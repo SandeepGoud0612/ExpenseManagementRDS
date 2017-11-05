b48dc3ccb24825ecd92a08116a4911d603a841dc
sandeep <sandeepgoud0612@gmail.com>
2017-11-04 23:47:35 -0400

    Initial Commit

---
+++ src/main/java/com/sam/ExpenseManagementRdsApplication.java
@@ -0,0 +1,12 @@
+package com.sam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagementRdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseManagementRdsApplication.class, args);
+	}
+}
